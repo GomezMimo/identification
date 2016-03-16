f90715025d57d643cb400cdacbb861de3d8d8fac
Juan Gomez <juan.camilo.mimo@gmail.com>
2016-03-16 01:39:52 -0500

    Add buttons and 'generate signature' functionality

--- Identification/scripts/identification.js
+++ Identification/scripts/identification.js
@@ -1,18 +1,25 @@
 $(document).ready(function(){	
+	var signature_ = "";
 	$('#firstname').on('keyup', function(){
-		var valor  = $('#firstname').val();
-		$('#firstname_text').text(" " + valor);		
+		var value  = $('#firstname').val();				
+		$('#firstname_text').text(" " + value);
 	});
 	$('#lastname').on('keyup', function(){
-		var valor  = $('#lastname').val();
-		$('#lastname_text').text(" " + valor);		
+		var value  = $('#lastname').val();		
+		$('#lastname_text').text(" " + value);				
 	});
 	$('#id').on('keyup', function(){
-		var valor  = $('#id').val();
-		$('#id_text').text(" " + valor);		
+		var value  = $('#id').val();
+		$('#id_text').text(" " + value);		
 	});
 	$('#country').on('keyup', function(){
-		var valor  = $('#country').val();
-		$('#country_text').text(" " + valor + "'s republic");		
+		var value  = $('#country').val();
+		$('#country_text').text(" " + value + "'s republic");		
+	});
+
+	$('#signature_button').on('click', function(){				
+		var name = $('#firstname').val();
+		var lastname = $('#lastname').val();
+		$('#signature').text(name + " " + lastname);
 	});
 });
